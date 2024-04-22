19a591f105dc89d28326779f2e417c8334c57a77
aishatariq1980 <167800892+aishatariq1980@users.noreply.github.com>
2024-04-22 22:44:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let favNumber = 7;
+let message = "mine favourite number is :";
+console.log(message);
+console.log(favNumber);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let favNumber = 7 ;
+let message ="mine favourite number is :" ;
+console.log(message);
+console.log(favNumber);
